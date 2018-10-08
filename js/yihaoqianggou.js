eac7d7555587d51c4265eb002f247b407df0d054
jinyalun <2295935102@qq.com>
2018-10-08 19:58:50 +0800

    jyl/项目更加简洁

--- js/yihaoqianggou.js
+++ js/yihaoqianggou.js
@@ -13,35 +13,43 @@ $(function(){
         $(this).find("img").animate({width:"100%",
         height:'100%',})
     })
-    //浮出加入购物车
-    //获取元素想对于文档左上角的距离的函数
-function getOffsetPosition(dom){
-    var left=0,t=0;
-    while(dom.offsetParent){
-        t += dom.offsetTop;
-        left += dom.offsetLeft;
-        dom = dom.offsetParent;
-    }
-    var o = {left:left,top:t};
-    return o;
-}
- //当body或HTML被卷起的值大于或等于该元素相对于文档左上角的高度值时seckill_header固定定位
- document.documentElement=document.body.onscroll=function(){
-    var seckill_container=document.getElementsByClassName("seckill_container")[0]
-    var t=getOffsetPosition(seckill_container).top;
-    var seckill_header=document.getElementsByClassName("seckill_header")[0]
-    // console.log(t)
-    var yihaoqianggou=document.getElementsByClassName("yihaoqianggou")[0]
-    console.log(t)
-    if(document.documentElement.scrollTop >= t){
-        seckill_header.className="seckill_header fixed";
-        yihaoqianggou.style.display="block";
+//     //浮出加入购物车
+//     //获取元素想对于文档左上角的距离的函数
+// function getOffsetPosition(dom){
+//     var left=0,t=0;
+//     while(dom.offsetParent){
+//         t += dom.offsetTop;
+//         left += dom.offsetLeft;
+//         dom = dom.offsetParent;
+//     }
+//     var o = {left:left,top:t};
+//     return o;
+// }
+//  //当body或HTML被卷起的值大于或等于该元素相对于文档左上角的高度值时seckill_header固定定位
+//  document.documentElement=document.body.onscroll=function(){
+//     var seckill_container=document.getElementsByClassName("seckill_container")[0]
+//     var t=getOffsetPosition(seckill_container).top;
+//     var seckill_header=document.getElementsByClassName("seckill_header")[0]
+//     var yihaoqianggou=document.getElementsByClassName("yihaoqianggou")[0]
+//     console.log(t)
+//     if(document.documentElement.scrollTop >= t){
+//         seckill_header.className="seckill_header fixed";
+//         yihaoqianggou.style.display="block";
+//     }else{
+//         console.log(1)
+//         seckill_header.className="seckill_header";
+//         yihaoqianggou.style.display="none";
+//     }
+// }
+$(document).on("scroll",function(){
+    var t=$(".seckill_container").offset().top
+    if($(document).scrollTop()>=t){
+        $('.seckill_header').addClass('fixed')
+        $('.yihaoqianggou').css("display","block")
     }else{
-        console.log(1)
-        seckill_header.className="seckill_header";
-        yihaoqianggou.style.display="none";
+        $('.seckill_header').removeClass('fixed')
+        $('.yihaoqianggou').css("display","none")
     }
-}
-
+})
 })
 
