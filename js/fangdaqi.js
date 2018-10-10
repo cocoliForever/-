88a918b69e48b55087bd72933bf1de0200fc2758
jinyalun <2295935102@qq.com>
2018-10-10 20:34:52 +0800

    jyl/放大器缺完善

--- js/fangdaqi.js
+++ js/fangdaqi.js
@@ -3,8 +3,53 @@
        $(".mBox").on("mouseenter","b",function(){
            $(this).addClass("cur").siblings().removeClass('cur')
            var imgssss = $(this).children(".detail_main_pic_class").attr("src")
-           console.log(imgssss)
-           $("#J_prodImg").attr("src",imgssss) 
+           $("#J_prodImg").attr("src",imgssss)
+           $("#J_zoom").children("img").attr("src",imgssss)
+           $("#J_zoom").css({display:"none"})
+
         }) 
+        $('.mask').on('mousemove',function(eve){
+            // console.log($(this).offset().left)
+
+            var x1=eve.clientX-$(this).offset().left
+            var y1=eve.clientY-$(this).offset().top
+            var x=x1-90;
+            var y=y1-90;
+            if(x<0){
+                x=0
+            }
+            if(x>=180){
+                x=180
+            }
+            if(y<0){
+                y=0
+            }
+            if(y>=180){
+                y=180
+            }
+            $(".zoomCursor").css({
+                'display':'block',
+                'left':x,
+                'top':y
+            })
+                
+            $("#J_zoom").css({
+                'display':'block'
+            }).children('img').css({
+                'top':-2*y,
+                'left':-2*x,
+            })
+        })
+        $('.mask').on('mouseleave',function(){
+            $(".zoomCursor").css({
+                'display':'none'
+            })
+            $("#J_zoom").css({
+                'display':'none'
+            })
+
+        })
+
+
     })
 }(jQuery))
