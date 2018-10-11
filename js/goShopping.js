864eff7c31f7d978aab547d79717f6ed16d2cd17
COCOLI <cocoli1281732513@gmail.com>
2018-10-11 22:44:36 +0800

    完善购物车抛物线特效

--- js/goShopping.js
+++ js/goShopping.js
@@ -1,41 +1,41 @@
-$(function () {
+$( function () {
   //倒计时========================================
-  var starttime = new Date("2018/11/20");
-  var starttime1 = new Date("2018/12/12");
+  var starttime = new Date( "2018/11/20" );
+  var starttime1 = new Date( "2018/12/12" );
   //这里声明到期时间
-  function p(s) {
+  function p ( s ) {
     return s < 10 ? '0' + s : s;
   }
-  setInterval(function () {
+  setInterval( function () {
     var nowtime = new Date();
-    function timer(time, parmas) {
-      var day = parseInt(time / 1000 / 60 / 60 / 24);
-      var hour = parseInt(time / 1000 / 60 / 60 % 24);
-      var minute = parseInt(time / 1000 / 60 % 60);
-      var seconds = parseInt(time / 1000 % 60);
-      $(parmas).html(p(day) + " " + p(hour) + ':' + p(minute) + ":" + p(seconds));
+    function timer ( time, parmas ) {
+      var day = parseInt( time / 1000 / 60 / 60 / 24 );
+      var hour = parseInt( time / 1000 / 60 / 60 % 24 );
+      var minute = parseInt( time / 1000 / 60 % 60 );
+      var seconds = parseInt( time / 1000 % 60 );
+      $( parmas ).html( p( day ) + " " + p( hour ) + ':' + p( minute ) + ":" + p( seconds ) );
 
     }
-    (function () {
+    ( function () {
       var time = starttime - nowtime;
       var time1 = starttime1 - nowtime;
       // 这里声明时间差值
       // 下面调用，第二个参数为要填入的地方的类名
-      timer(time, '.countdown');
-      timer(time1, '.countlast')
-    }())
-  }, 1000);
-})
+      timer( time, '.countdown' );
+      timer( time1, '.countlast' )
+    }() )
+  }, 1000 );
+} )
 
-$(function () {
+$( function () {
   /**
- * 
+ *
  * 获取当前时间
  */
-  function p(s) {
+  function p ( s ) {
     return s < 10 ? '0' + s : s;
   }
-  setInterval(function () {
+  setInterval( function () {
 
     var myDate = new Date();
     //获取当前年
@@ -48,41 +48,42 @@ $(function () {
     var m = myDate.getMinutes();     //获取当前分钟数(0-59)
     var s = myDate.getSeconds();
 
-    var $now = year + '-' + p(month) + "-" + p(date) + " " + p(h) + ':' + p(m) + ":" + p(s);
-    $('#nowTime').html($now);
-  }, 1000)
-})
-var num, wl, hl, s;
-$(function () {
-  $('.del_btn').on('click', function () {
-    var $a = $(this).parents('.main_item.item_line');
+    var $now = year + '-' + p( month ) + "-" + p( date ) + " " + p( h ) + ':' + p( m ) + ":" + p( s );
+    $( '#nowTime' ).html( $now );
+  }, 1000 )
+} )
+var wl, hl, s;
+$( function () {
+  $( '.del_btn' ).on( 'click', function () {
+    var $a = $( this ).parents( '.main_item.item_line' );
     height = $a.offset().top
     left = $a.offset().left
     width = $a.width() / 2,
-      fleft = $('.fixde_shop').offset().left
-    ftop = $('.fixde_shop').offset().top
+      fleft = $( '.fixde_shop' ).offset().left
+    ftop = $( '.fixde_shop' ).offset().top
     wl = fleft - left - width
     hl = ftop - height
-    $(this).parents('.main_item.item_line').css({
+    $( this ).parents( '.main_item.item_line' ).css( {
       transform: 'scale(0.2)',
       transition: 'all .8s'
-    })
-    num = $(this).index();
-    setTimeout(function () {
-      $('.del_btn').eq(0).parents('.main_item').appendTo('.fixde_shop').addClass('active').css({ 'left': -wl, 'top': -hl }).animate({
+    } )
+    var $url = $a.find( '.mainCart' ).attr( 'src' )
+    setTimeout( function () {
+      $( '.del_btn' ).eq( 0 ).parents( '.main_item' ).empty().appendTo( '.fixde_shop' ).addClass( 'active' ).css( {
+        'left': -wl, 'top': -hl, width: 100, height: 100, transform: '',
+        transition: '', 'background-image': 'url(' + $url + ')'
+      } ).animate( {
         left: -40,
         top: -40
       }, function () {
-        var s = parseInt($(this).css('top'))
-        console.log(s)
-        if (s === -54) {
-          console.log(11121515)
-          $(this).hide(2000)
-        }
-      })
 
-    }, 900)
 
-  })
+        $( this ).fadeOut( 2000 )
 
-})
+      } )
+
+    }, 900 )
+
+  } )
+
+} )
