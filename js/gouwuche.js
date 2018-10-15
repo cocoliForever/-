0433b56cf37e60dda3cf310fabdb29c176aebdeb
zhoujunjie <41522402@qq.com>
2018-10-15 23:35:07 +0800

    zjj-gouwuche

--- js/gouwuche.js
+++ js/gouwuche.js
@@ -1,19 +1,22 @@
 ;(function($){
     $(function(){
-        $('.cart_list').on('click','.prop_txt',function(){
+        
+        $('.cart_list').on('mouseover','.prop_txt',function(){
              
+            $(this).css({
+                'border':'1px dashed red'
+            })
+        }).on('mouseout','.prop_txt',function(){
+            $(this).css({
+                'border':'1px dashed #fff'
+            })
+        })
+        $('.cart_list').on('click','.prop_txt',function(){
             $(this).next().slideToggle()
             $(this).css({
                 'border':'1px dashed red'
             })
-            console.log($(this).css('border'))
-            if($(this).css('border','1px dashed rgb(255, 0, 0)')){
-        
-            }
-           
-        
         })
-
 
 
 
