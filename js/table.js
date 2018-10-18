62d32401c0f03a8d3ce2bbadec734646d772cc80
Anyone <1169493271@qq.com>
2018-10-18 17:37:51 +0800

    zcq

--- js/table.js
+++ js/table.js
@@ -1,5 +1,5 @@
 $(function(){
-    // 点击更多使相关分类中内容全部展示
+// 点击更多使相关分类中内容全部展示****************
     $('.classWrap .multiple_choice a').on('click',function(){
         var num = parseInt($('.guide_con_noLabel').css('height'))
         if(num===80){
@@ -13,10 +13,11 @@ $(function(){
     // 生成jspDrag
     var $jspDrag=$('<div class="jspDrag"></div>')
       
-      // 点击更多使相关分类中内容全部展示
-     
+
+// 品牌***************************************** 
     $('.brandWrap .multiple_choice a').on('click',function(){
         var m=parseInt($('.guide_lists').css('height'))
+        // 添加滚动条
         if(m===107){
             $('.brandWrap .guide_lists').css('height',213)
             $('.brandWrap .guide_ul').css( 'width',1270 )
@@ -40,6 +41,7 @@ $(function(){
         }
          // 向下滑动jspDrag，使内容跟着滑动
         $('.jspTrack').append($jspDrag)
+                 // 品牌展开下的事件
         if( parseInt($('.jspTrack').css('height'))==213){
             $('.jspDrag').on('mousedown',function(e) {　
                 var positionY = e.pageY - $(this).offset().top;
@@ -63,33 +65,54 @@ $(function(){
                     $(document).off('mousemove');
                 })
             })
-            // 点击内容使内容呈现在已选之中
+                // 点击品牌li事件************************
             $('.jspContainer .guide_ul').on('click','li',function(){
+                console.log($(this).find('.sl'))
+               
+                // 点中li，border显示红色，再次点击颜色消失
+                if($(this).find('.sl').css('display')==='block'){
+                    $(this).find('.sl').css({'display':'block'})
+                    $(this).find('.sr').css({'display':'block','color':'#fff'})
+                    $(this).find('.cl').css('display','block')
+                }else{
+                    $(this).find('.sl').css({'display':'none'})
+                    $(this).find('.sr').css({'display':'block','color':'#333'})
+                    $(this).find('.cl').css('display','none')
+                }
+               // 点击内容使内容呈现在已选之中 
                var $span=$('<div class="ansery">'+$(this).find('.sl').text()+'<span class="close">×</span></div>')
                 $('.guide_selected').append($span)
+                $('.close').on('click',function(){
+                    $(this).parent().remove()
+                    $('guide_ul .li').find('.sl').css('borderColor','#fff') 
+                    if($('.ansery').length===0){
+                        // 确定按钮变化
+                        $('.btn_ok').css({
+                            'cursor': 'not-allowed',
+                            'color': '#999',
+                            'borderColor':'#cfcfcf',
+                            'background':' #fbfbfb'
+                        })
+                    }
+                    // console.log($(document).text($(this).parent().text()))
+                 })
                 $('.btn_ok').css({
                     'cursor':'auto',
                     'backgroundColor':'#ff855c',
                     'borderColor':'#f27e57',
                     'color':'#fff'
                 })
-                $('.close').on('click',function(){
-                    $span.remove()
-                })
-                $('.btn_cancel').on('click',function(){
-                    
-                    $span.remove()
-                    $('.btn_ok').css({
-                        'cursor':'not-allowed',
-                        'backgroundColor':'#fbfbfb',
-                        'borderColor':'#cfcfcf',
-                        'color':'#999'
-                    })
-                    $('.brandWrap .guide_lists').css('height',107)
-                })
             })
-           
+            // 点击取消事件
+            $('.btn_cancel').on('click',function(){
+                $('.guide_switch').css('display','none')
+                $('.guide_selected').css('display','none')
+                $('.brandWrap .guide_ul').css( 'width',1156 )
+                $('.brandWrap .guide_btn').css('display','none')
+                $('.brandWrap .guide_lists').css('height',107)       
+            })
         }
+        // 品牌未展开时
         if( parseInt($('.jspTrack').css('height'))==107){
             $('.jspDrag').on('mousedown',function(e) {　
                 var positionY = e.pageY - $(this).offset().top;
@@ -113,7 +136,7 @@ $(function(){
             })
         }
     })
-    
+ 
         
     
 
