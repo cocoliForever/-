7885a50d8f425385785ffa128a0e34b5e541db4d
deephug <m18537052289@163.com>
2018-10-24 10:58:42 +0800

    cx-完成项目

--- js/wodeyihaodian.js
+++ js/wodeyihaodian.js
@@ -44,12 +44,49 @@
       })
     })
 
-    // $('.nav_item').eq(2).on('mouseenter',function(){
-    //   $('.pull_list_wrap').css('display','block')
-    // })
-
+    // 搜素框focus的时候
+    $('.enter_box').focus(function(){
+      $(this).css('background','white').parent().css('background','white')
+    })
+    // 搜索框blur的时候
+    $('.enter_box').blur(function(){
+      $(this).css('background','#514f57').parent().css('background','#514f57')
+    })
     
 
+
+  
+    /**
+   *
+   * 获取当前时间
+   */
+    function p(s) {
+      return s < 10 ? '0' + s : s;
+    }
+    setInterval(function () {
+
+      var myDate = new Date();
+      //获取当前年
+      var year = myDate.getFullYear();
+      //获取当前月
+      var month = myDate.getMonth() + 1;
+      //获取当前日
+      var date = myDate.getDate();
+      var h = myDate.getHours();       //获取当前小时数(0-23)
+      var m = myDate.getMinutes();     //获取当前分钟数(0-59)
+      var s = myDate.getSeconds();     //获取当前秒(0-59) 
+
+      var $now = year + '-' + p(month) + "-" + p(date) + " " + p(h) + ':' + p(m) + ":" + p(s);                      //年月日时分秒
+      // var $now = year + '-' + p(month) + "-" + p(date);
+      $('.user_account_date .date').html($now);
+    }, 1000)
+    
+    
+    // 获取星期几
+    var mydate = new Date();
+    var today = new Array('星期一','星期二','星期三','星期四','星期五','星期六','星期日');
+    var week = today[mydate.getDay()-1];
+    $('.user_account_date .week').html(week)
 
 
 
