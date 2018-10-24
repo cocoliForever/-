a68b4c4a949b6c11f4bdb5bd52ea3cf684eca120
Anyone <1169493271@qq.com>
2018-10-24 20:31:45 +0800

    zcq

--- js/table.js
+++ js/table.js
@@ -30,6 +30,8 @@ $(function(){
             $('.guide_selected').css('display','block')
             $('.brandWrap .guide_btn').css('display','block')
             $('.brandWrap .multiple_choice .many').html('收起 <i class=" iconfont icon-xiangshang amine"></i>')
+             // 向下滑动jspDrag，使内容跟着滑动
+            
         }else{
             $('.brandWrap .guide_lists').css( 'height','')
             $('.brandWrap .over_any').css('display','inline')
@@ -41,17 +43,15 @@ $(function(){
             $('.brandWrap .guide_btn').css('display','none')
             $('.brandWrap .multiple_choice .many').html('更多 <i class=" iconfont icon-xiangxia amine"></i>')
         }
-    })
-         // 向下滑动jspDrag，使内容跟着滑动
         $('.jspTrack').append($jspDrag)
+    })
+        
                  // 品牌展开下的事件
-        if( parseInt($('.jspTrack').css('height'))==213){
-            console.log(122)
-            $('.jspDrag').on('mousedown',function(e) {　
+      
+            $('.jspTrack').on('mousedown','.jspDrag',function(e) {　
                 var positionY = e.pageY - $(this).offset().top;
                 $(document).on('mousemove',function(e) {　　
             　　　　var divY = e.clientY - positionY;
-            console.log(123)
                     if(divY<=135){
                         divY=135;
                     }
@@ -70,10 +70,9 @@ $(function(){
                     $(document).off('mousemove');
                 })
             })
-        } // 品牌未展开时
-        if( parseInt($('.jspTrack').css('height'))==107){
-            console.log(123)
-            $('.jspDrag').on('mousedown',function(e) {　
+         // 品牌未展开时
+         $('.jspTrack').on('mousedown','.jspDrag',function(e) {
+                if( parseInt($('.jspTrack').css('height'))==107){
                 var positionY = e.pageY - $(this).offset().top;
                 $(document).on('mousemove',function(e) {　　
             　　　　var divY = e.clientY - positionY;
@@ -84,7 +83,7 @@ $(function(){
                         divY=230;
                     }
                     $('.jspDrag').css({
-                        'top':divY-135
+                        'top':divY-100
                     })
                     var haa = parseInt((6.6)*divY)
                     $('.jspContainer').css('top',-haa+660)
@@ -92,71 +91,72 @@ $(function(){
                 $(document).on('mouseup',function(){
                     $(document).off('mousemove');
                 })
+            }
+        })
+        // 点击品牌li事件************************
+    $('.jspContainer .guide_ul').on('click','li',function(){
+        // console.log($(this).find('.sl'))
+        // 点击内容使内容呈现在已选之中 
+        if( parseInt($('.jspTrack').css('height'))==213){
+            var index=$(this).data('index')
+            console.log(index)
+            var $span=$('<div class="ansery" data-to="'+index+'"><span class="cont">'+$(this).find('.sl').text()+'</span><span class="close">×</span></div>')
+            $('.guide_selected').append($span)
+            // 遍历ansery若有相同内容的盒子，删去
+            $('.ansery').each(function(){
+                if($(this).text() == $(this).prev().text()){
+                    $(this).remove();
+                }
+            })
+            // 点中li，border显示红色，再次点击颜色消失
+            if($(this).find('.cl').css('display')==='none'){
+                
+                $(this).find('.sr').addClass('sy')
+                $(this).find('.sl').addClass('al')
+                $(this).find('.cl').show()
+            }else{
+                $(this).find('.sr').removeClass('sy')
+                $(this).find('.sl').removeClass('al')
+                $(this).find('.cl').hide()
+                $('.ansery[data-to="'+index+'"]').remove()
+            } 
+            if($('.ansery').length>=1){
+                $('.btn_ok').css({
+                'cursor':'auto',
+                'backgroundColor':'#ff855c',
+                'borderColor':'#f27e57',
+                'color':'#fff'
+                })
+            }
+        }
+    })
+    // 点击确定按钮刷新页面
+    $('.guide_btn').on('click','.btn_ok',function(){
+        if($('.btn_ok').css('backgroundColor')=='rgb(255, 133, 92)'){
+            window.location.reload()
+        }
+    })
+    $('.guide_selected').on('click','.close',function(){
+        $(this).parents('.ansery').remove()
+        $('guide_ul .li').find('.sl').css('borderColor','#fff') 
+        if($('.ansery').length===0){
+            // 确定按钮变化
+            $('.btn_ok').css({
+                'cursor': 'not-allowed',
+                'color': '#999',
+                'borderColor':'#cfcfcf',
+                'background':' #fbfbfb'
             })
         }
-                // 点击品牌li事件************************
-            $('.jspContainer .guide_ul').on('click','li',function(){
-                // console.log($(this).find('.sl'))
-               // 点击内容使内容呈现在已选之中 
-               var index=$(this).data('index')
-               console.log(index)
-               var $span=$('<div class="ansery" data-to="'+index+'"><span class="cont">'+$(this).find('.sl').text()+'</span><span class="close">×</span></div>')
-               $('.guide_selected').append($span)
-               $('.ansery').each(function(){
-                    if($(this).text() == $(this).prev().text()){
-                        $(this).remove();
-                    }
-                })
-                 // 点中li，border显示红色，再次点击颜色消失
-                 if($(this).find('.cl').css('display')==='none'){
-            
-                    // moseenter()
-                    $(this).find('.cl').show()
-                }else{
-                //    mouseleave()
-                    $(this).find('.cl').hide()
-                    $('.ansery[data-to="'+index+'"]').remove()
-               
-                } 
-               if($('.ansery').length>=1){
-                   $('.btn_ok').css({
-                    'cursor':'auto',
-                    'backgroundColor':'#ff855c',
-                    'borderColor':'#f27e57',
-                    'color':'#fff'
-                   })
-               }
-               console.log($('.ansery'))
-            })
-            $('.guide_selected').on('click','.close',function(){
-                $(this).parents('.ansery').remove()
-                $('guide_ul .li').find('.sl').css('borderColor','#fff') 
-                if($('.ansery').length===0){
-                    // 确定按钮变化
-                    $('.btn_ok').css({
-                        'cursor': 'not-allowed',
-                        'color': '#999',
-                        'borderColor':'#cfcfcf',
-                        'background':' #fbfbfb'
-                    })
-                }
-                // console.log($(document).text($(this).parent().text()))
-                })
-               
-            
-           
-            // 点击取消事件
-            $('.btn_cancel').on('click',function(){
-                $('.guide_switch').css('display','none')
-                $('.guide_selected').css('display','none')
-                $('.brandWrap .guide_ul').css( 'width',1156 )
-                $('.brandWrap .guide_btn').css('display','none')
-                $('.brandWrap .guide_lists').css('height',107)       
-            })
-        
-       
-        
-    
+        })
+    // 点击取消事件
+    $('.btn_cancel').on('click',function(){
+        $('.guide_switch').css('display','none')
+        $('.guide_selected').css('display','none')
+        $('.brandWrap .guide_ul').css('width',1156)
+        $('.brandWrap .guide_btn').css('display','none')
+        $('.brandWrap .guide_lists').css('height',107)       
+    })
     // 品牌实现tab切换
     $('.guide_switch').on('click','span',function(){
         $(this).addClass('cur').siblings('.cur').removeClass('cur')
@@ -173,20 +173,6 @@ $(function(){
             $('.guide_ul li[data-type="other"]').fadeIn(100)
         }
     })
-        
-    var moseenter=function(){
-        $('.guide_ul li').on('mouseenter',function(){
-            $(this).find('.sl').fadeIn(0)
-            $(this).find('.sr').fadeOut(0)
-        })
-    }
-    var mouseleave=function(){
-         $('.guide_ul li').on('mouseleave',function(){
-            $(this).find('.sl').fadeOut(0)
-            $(this).find('.sr').fadeIn(0)
-        })
-    }    
-       
     })
     $(function(){
         // 点击多选事件
@@ -219,24 +205,35 @@ $(function(){
             } 
          })
         //  点击ul中li，精灵图变为对号
-         $('.search_guide ').on('click','.guide_main .guide_con li',function(){ 
-            // console.log($(this).children().find('i'))
-             $(this).children().find('i').css('backgroundPositionX',-75)
-             $(this).children().find('i').css('backgroundPositionY',-58)
-             $(this).children().find('span').css('color','#ff4040')
-            //  console.log($(this).parent().next('.guide_btn').children(0))
-            // btn_ok变化
-             $(this).parent().next('.guide_btn').children().first().css({
-                'cursor':'auto',
-                'backgroundColor':'#ff855c',
-                'borderColor':'#f27e57',
-                'color':'#fff'
-            })
+         $('.search_guide ').on('click','.guide_con li',function(){ 
+            //  if( $(this).find('.guide_box').css('height')==="auto"){
+                if($(this).children().find('i').css('backgroundPositionX')==-64+"px"){
+                    $(this).children().find('i').css('backgroundPositionX',-75)
+                    $(this).children().find('i').css('backgroundPositionY',-58)
+                    $(this).children().find('span').css('color','#ff4040')
+                    $(this).parents('.guide_main').find('.btn_ok').css({
+                        'cursor':'auto',
+                        'backgroundColor':'#ff855c',
+                        'borderColor':'#f27e57',
+                        'color':'#fff'
+                    })
+                }else{
+                    $(this).children().find('i').css('backgroundPositionX',-64)
+                    $(this).children().find('i').css('backgroundPositionY',-70)
+                    $(this).children().find('span').css('color','#333')
+                    $(this).parents('.guide_main').find('.btn_ok').css({
+                        'cursor': 'not-allowed',
+                        'color': '#999',
+                        'borderColor':'#cfcfcf',
+                        'background':' #fbfbfb'
+                    })
+                }
+            // }  
             // 点击btn_ok事件
             $(this).parent().next('.guide_btn').children().first().on('click',function(){
                 window.location.reload()
             })
-            // 点击btn_cancel事件
+            // 点击取消btn_cancel事件
             $(this).parent().next('.guide_btn').children().last().on('click',function(){
                 // console.log(1)
                 $(this).parent().prev().children().children().find('span').css('color','#333')
@@ -270,12 +267,14 @@ $(function(){
             $(this).parents('.son_m').find('.guide_btn').css('display','block')
             $(this).hide()
         })
+        // if( $(this).parents('.son_m').find('.guide_btn').css('display','block')){
+//  点击som_an中a，精灵图变为对号
         $('.son_m').on('click','.som_an a',function(){
             if($(this).find('.sub').css('backgroundPositionX')===-64+"px"){
                 $(this).find('.sub').css('backgroundPositionX',-75)
                 $(this).find('.sub').css('backgroundPositionY',-58)
                 $(this).find('span').css('color','#ff4040')
-                $(this).parent().next().children().first().css({
+                $(this).parents('.son_m').find('.btn_ok').css({
                     'cursor':'auto',
                     'backgroundColor':'#ff855c',
                     'borderColor':'#f27e57',
@@ -285,19 +284,21 @@ $(function(){
                 $(this).find('.sub').css('backgroundPositionX',-64)
                 $(this).find('.sub').css('backgroundPositionY',-70)
                 $(this).find('span').css('color','#333')
-                $(this).parent().next().children().first().css({
+                $(this).parents('.son_m').find('.btn_ok').css({
                     'cursor': 'not-allowed',
                     'color': '#999',
                     'borderColor':'#cfcfcf',
                     'background':' #fbfbfb'
                 })
-            }  
+            } 
+        // } 
         })
+        // 点击确定刷新页面
         $('.son_m').on('click','.btn_ok ',function(){
             window.location.reload()
         })
+        // 点击取消事件
         $('.son_m').on('click','.btn_cancel',function(){    
-            console.log($(this))  
             $(this).parent().css('display','none')
             $(this).parents('.son_m').find('.over_mush').css('display','block')
             $(this).parents('.son_m').find('.som_an span').css('color','#333')
